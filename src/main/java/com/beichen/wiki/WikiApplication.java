a00984be19e08f4059bef5bb50c5a322b76670b4
amazingsbc <1150311203@qq.com>
2021-08-16 22:07:14 +0800

    2-5 修改启动文案

--- src/main/java/com/beichen/wiki/WikiApplication.java
+++ src/main/java/com/beichen/wiki/WikiApplication.java
@@ -1,13 +1,22 @@
 package com.beichen.wiki;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.slf4j.Logger;
 
 @SpringBootApplication
 public class WikiApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+//        SpringApplication.run(WikiApplication.class, args);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+
     }
 
 }
