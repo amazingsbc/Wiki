51031d62d7249db94678d50391618fdd4f9c7a51
amazingsbc <1150311203@qq.com>
2021-08-18 16:51:20 +0800

    2-7 增加hello测试接口 test.http

--- src/main/java/com/beichen/wiki/controller/TestController.java
+++ src/main/java/com/beichen/wiki/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.beichen.wiki.controller;
 
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,8 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello(){
         return "Hello world";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(String map){
+        return "hello world " + map;
     }
 }
